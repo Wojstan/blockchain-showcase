ac2be383159d5465f9d40d8bad50fcfc13737792
Wojstan <wpstaniszewski@gmail.com>
2025-02-05 00:02:14 +0100

    Lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { WagmiProvider } from 'wagmi'
-import { config } from './config/wagmiConfig'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { WalletConnect } from './components/WalletConnect'
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { WagmiProvider } from 'wagmi'
+import { WalletConnect } from './components/WalletConnect'
+import { config } from './config/wagmiConfig'
 
 const queryClient = new QueryClient()
 
@@ -15,7 +15,5 @@ const App = () => (
     </QueryClientProvider>
   </WagmiProvider>
 )
-
-
 
 export default App
--- src/config/rainbowConfig.ts
+++ src/config/rainbowConfig.ts
@@ -1,8 +1,8 @@
-import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { mainnet, sepolia } from "wagmi/chains";
+import { getDefaultConfig } from '@rainbow-me/rainbowkit'
+import { mainnet, sepolia } from 'wagmi/chains'
 
 export const rainbowConfig = getDefaultConfig({
-  appName: "My RainbowKit App",
-  projectId: "YOUR_PROJECT_ID",
+  appName: 'My RainbowKit App',
+  projectId: 'YOUR_PROJECT_ID',
   chains: [mainnet, sepolia],
-});
+})
