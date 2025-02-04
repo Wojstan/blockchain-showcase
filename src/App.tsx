2e9374e9726d9311a8408774bbe2ed96690cd47a
Wojstan <51679670+Wojstan@users.noreply.github.com>
2025-02-05 00:04:09 +0100

    Connect wallet using RainbowKit (#2)
    * Add WalletConnector
    
    * Lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
-function App() {
-  return <div>Empty project</div>
-}
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { WagmiProvider } from 'wagmi'
+import { WalletConnect } from './components/WalletConnect'
+import { config } from './config/wagmiConfig'
+
+const queryClient = new QueryClient()
+
+const App = () => (
+  <WagmiProvider config={config}>
+    <QueryClientProvider client={queryClient}>
+      <RainbowKitProvider>
+        <WalletConnect />
+      </RainbowKitProvider>
+    </QueryClientProvider>
+  </WagmiProvider>
+)
 
 export default App
